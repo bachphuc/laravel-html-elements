907743858eb0bf5cf6c33a726711e9f3a072b23a
Bach Phuc <dmobi.dsoft@gmail.com>
2020-04-12 15:49:00 +0700

    - Init package.

---
+++ src/Index.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace backphuc\LaravelHTMLElements;
+
+class Information{
+    public function display(){
+        return [
+            'author' => 'bachphuc',
+            'email' => 'trieuquangphuc123@gmail.com'
+        ];
+    }
+}
